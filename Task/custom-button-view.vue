12c6aba1289f77e714a124ebcd84a9d934052967
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2023-03-19 15:07:51 +0200

    Add files via upload

---
+++ Task/custom-button-view.vue
@@ -0,0 +1,33 @@
+<template>
+    <button  :class="['button', type]" >
+        <slot>Click</slot>
+    </button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'MyButton',
+    props: {
+        type: {
+            type: String,
+            default: '',
+        },
+    },
+});
+</script>
+<style lang="less">
+.button {
+    padding: 8px;
+    background-color: #0077ff;
+    border-radius: 4px;
+    color: #fff;
+    font-weight: bold;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #0059b3;
+    }
+}
+</style>
+  
---
+++ Task/custom-input-view.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="input-wrapper">
+        <label :for="name">{{ name }}</label>
+        <div class="input-container">
+            <input :type="type" :placeholder="placeholder" v-model="value" />
+            <button v-if="showClearButton" class="clear-button" @click="clearInput">
+                <i class="material-icons">clear</i>
+            </button>
+            <i v-if="icon" class="material-icons icon">{{ icon }}</i>
+        </div>
+    </div>
+</template>
+  
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+    name: 'MyInput',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+        placeholder: {
+            type: String,
+            default: '',
+        },
+        icon: {
+            type: String,
+            default: '',
+        },
+        showClearButton: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup(props, { emit }) {
+        const clearInput = () => {
+            emit('update:modelValue', '');
+        };
+
+        return {
+            value: '',
+            clearInput,
+        };
+    },
+});
+</script>
+  
+<style scoped>
+.input-wrapper {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+}
+
+.input-container {
+    display: flex;
+    align-items: center;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 5px;
+}
+
+input {
+    border: none;
+    outline: none;
+    flex-grow: 1;
+}
+
+.clear-button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    margin-left: 5px;
+}
+
+.icon {
+    margin-right: 5px;
+}
+</style>
+  
